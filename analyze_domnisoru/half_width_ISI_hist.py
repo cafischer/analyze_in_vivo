39b0f3f4634b7c1d64fc6ac51ea27a9843a235a6
cafischer <coralinefischer@gmail.com>
2018-06-14 22:42:08 +0200

    plots domnisoru

--- analyze_domnisoru/half_width_ISI_hist.py
+++ analyze_domnisoru/half_width_ISI_hist.py
@@ -7,8 +7,17 @@ from grid_cell_stimuli.ISI_hist import get_ISIs, get_ISI_hist
 from analyze_in_vivo.load.load_domnisoru import load_cell_ids, load_data, get_celltype
 from analyze_in_vivo.analyze_domnisoru.check_basic.in_out_field import get_start_end_group_of_ones
 from scipy.ndimage.filters import convolve
+from scipy.optimize import curve_fit, brentq
 pl.style.use('paper')
 
+
+def fun_fit(x, a, b, c, tau):
+    res = c * (x - b) ** a * (np.exp(-(x - b) / tau))
+    if isinstance(res, (float, int)):
+        res = res if res > 0 else 0
+    else:
+        res[(x - b) <= 0] = 0
+    return res
 
 
 if __name__ == '__main__':
@@ -18,9 +27,12 @@ if __name__ == '__main__':
     save_dir_img = '/home/cf/Phd/programming/projects/analyze_in_vivo/analyze_in_vivo/results/domnisoru/whole_trace/ISI_hist'
     save_dir = '/home/cf/Phd/programming/projects/analyze_in_vivo/analyze_in_vivo/data/domnisoru'
     cell_type = 'grid_cells'
-    #cell_ids = load_cell_ids(save_dir, cell_type)
-    cell_ids = ['s43_0003', 's66_0003', 's67_0000', 's73_0004', 's76_0002', 's79_0003', 's82_0002', 's95_0006',
-                's101_0009', 's109_0002', 's117_0002', 's118_0002', 's119_0004', 's120_0002']
+    all_cells = True
+    if all_cells:
+        cell_ids = load_cell_ids(save_dir, cell_type)
+    else:
+        cell_ids = ['s43_0003', 's66_0003', 's67_0000', 's73_0004', 's76_0002', 's79_0003', 's82_0002', 's95_0006',
+                   's101_0009', 's109_0002', 's117_0002', 's118_0002', 's119_0004', 's120_0002']
     param_list = ['Vm_ljpc', 'spiketimes']
     AP_thresholds = {'s73_0004': -55, 's90_0006': -45, 's82_0002': -35,
                      's117_0002': -60, 's119_0004': -50, 's104_0007': -55,
@@ -28,16 +40,25 @@ if __name__ == '__main__':
     use_AP_max_idxs_domnisoru = True
     filter_long_ISIs = True
     max_ISI = 200
+    max_ISI_fit = 100
     n_smooth = 5
     if filter_long_ISIs:
         save_dir_img = os.path.join(save_dir_img, 'cut_ISIs_at_'+str(max_ISI))
 
     # parameter
-    bin_widths = np.arange(0.5, 2.0+0.1, 0.1)
+    bin_widths = [1.0]  #np.arange(0.5, 2.0+0.1, 0.1)  TODO
 
     # main
     half_width = np.zeros((len(cell_ids), len(bin_widths)))
     half_width_smoothed = np.zeros((len(cell_ids), len(bin_widths)))
+    ISI_at_peak = np.zeros((len(cell_ids), len(bin_widths)))
+    ISI_at_peak_smoothed = np.zeros((len(cell_ids), len(bin_widths)))
+    half_width_fit = np.zeros((len(cell_ids)))
+    ISI_at_peak_fit = np.zeros((len(cell_ids)))
+    ISI_hist_fit = []
+    root1_fit = np.zeros((len(cell_ids)))
+    root2_fit = np.zeros((len(cell_ids)))
+    p_opt_fit = []
     for cell_idx, cell_id in enumerate(cell_ids):
         for bin_idx, bin_width in enumerate(bin_widths):
             print cell_id
@@ -70,14 +91,47 @@ if __name__ == '__main__':
             len_group = ends[idx_group] - starts[idx_group] + 1
             half_width[cell_idx, bin_idx] = len_group * bin_width
 
-            # smooth half-width
-            half_width_smoothed[cell_idx, :] = convolve(half_width[cell_idx, :], np.ones(n_smooth)/float(n_smooth),
-                                                        mode='nearest')
-
-            # print half_width[cell_idx, bin_idx]
             # plot_ISI_hist(ISI_hist, bins, title=cell_id)
             # pl.plot([bins[starts[idx_group]], bins[ends[idx_group]]], [half_height, half_height], 'r')
             # pl.show()
+
+            # max_val
+            ISI_at_peak[cell_idx, bin_idx] = bins[bin_idx_peak]
+
+            # fit
+            if bin_width == 1.0:
+                bins_fit = np.arange(0, max_ISI_fit+bin_width, bin_width)
+                ISI_hist_fit.append(ISI_hist[(bins <= max_ISI_fit)[:-1]][:-1])
+
+                try:
+                    p_opt_ = curve_fit(fun_fit, bins_fit[:-1], ISI_hist_fit[cell_idx],
+                                       bounds=([0, 0, 0, 1e-8], [np.inf, np.inf, np.inf, np.inf]))[0]
+                    p_opt_fit.append(p_opt_)
+                except RuntimeError:
+                    p_opt_fit.append([np.nan, np.nan, np.nan, np.nan])
+
+                # half width of fit
+                ISI_at_peak_fit[cell_idx] = p_opt_fit[cell_idx][0] * p_opt_fit[cell_idx][3] + p_opt_fit[cell_idx][1]
+                half_height_fit = fun_fit(np.array([ISI_at_peak_fit[cell_idx]]), *p_opt_fit[cell_idx]) / 2.0
+                def fun_roots(x):
+                    return fun_fit(x, *p_opt_fit[cell_idx]) - half_height_fit
+                root1_fit[cell_idx] = brentq(fun_roots, 0, ISI_at_peak_fit[cell_idx])
+                if fun_roots(max_ISI_fit) >= 0:
+                    root2_fit[cell_idx] = brentq(fun_roots, ISI_at_peak_fit[cell_idx], max_ISI_fit+10)
+                else:
+                    root2_fit[cell_idx] = brentq(fun_roots, ISI_at_peak_fit[cell_idx], max_ISI_fit)
+                half_width_fit[cell_idx] = root2_fit[cell_idx] - root1_fit[cell_idx]
+
+                # pl.figure()
+                # pl.bar(bins_fit[:-1], ISI_hist_fit[cell_idx], bins_fit[1] - bins_fit[0], color='0.5')
+                # x = np.arange(0, 100, 0.01)
+                # pl.plot(x, fun_fit(x, *p_opt_fit[cell_idx]), '-', color='k', linewidth=1.5)
+                # pl.plot(ISI_at_peak_fit[cell_idx],
+                #         fun_fit(np.array([ISI_at_peak_fit[cell_idx]]), *p_opt_fit[cell_idx]), 'or')
+                # pl.plot([root1_fit[cell_idx], root2_fit[cell_idx]],
+                #         [fun_fit(root1_fit[cell_idx], *p_opt_fit[cell_idx]),
+                #          fun_fit(root2_fit[cell_idx], *p_opt_fit[cell_idx])], 'r', linewidth=2)
+                # pl.show()
 
             # # fit Gamma distribution to hist
             # import scipy.stats as stats
@@ -94,6 +148,12 @@ if __name__ == '__main__':
             # #pl.plot(bins_fit, stats.gamma.pdf(bins_fit, fit_alpha, fit_loc, fit_beta), 'r')
             # pl.plot(bins_fit, stats.invgauss.pdf(bins_fit, fit_a, fit_loc, fit_scale), 'r')
             # pl.show()
+
+        # smooth 
+        half_width_smoothed[cell_idx, :] = convolve(half_width[cell_idx, :], np.ones(n_smooth) / float(n_smooth),
+                                                    mode='nearest')
+        ISI_at_peak_smoothed[cell_idx, :] = convolve(ISI_at_peak[cell_idx, :], np.ones(n_smooth) / float(n_smooth),
+                                                    mode='nearest')
 
     cm = pl.cm.get_cmap('plasma')
     colors = cm(np.linspace(0, 1, len(cell_ids)))
@@ -117,8 +177,137 @@ if __name__ == '__main__':
                     horizontalalignment='left', verticalalignment='center', fontsize=8)
     # pl.legend(loc='upper left', fontsize=8)
     pl.xlim(bin_widths[0] - 0.05, bin_widths[-1] + 0.8)
-    pl.xlabel('Bin width')
-    pl.ylabel('ISI hist. width at half peak')
+    pl.xlabel('Bin width (ms)')
+    pl.ylabel('ISI hist. width at half peak (ms)')
     pl.tight_layout()
-    pl.savefig(os.path.join(save_dir_img, cell_type, 'half_width_smooth_'+str(n_smooth)+'.png'))
-    pl.show()
+    if all_cells:
+        pl.savefig(os.path.join(save_dir_img, cell_type, 'half_width_smooth_'+str(n_smooth)+'_all_cells.png'))
+    else:
+        pl.savefig(os.path.join(save_dir_img, cell_type, 'half_width_smooth_' + str(n_smooth) + '.png'))
+
+    # ISI at peak
+    cm = pl.cm.get_cmap('plasma')
+    colors = cm(np.linspace(0, 1, len(cell_ids)))
+    ISI_at_peak_smoothed_labels = {}
+    for cell_idx in range(len(cell_ids)):
+        if ISI_at_peak_smoothed_labels.get(ISI_at_peak_smoothed[cell_idx, -1], None) is None:
+            ISI_at_peak_smoothed_labels[ISI_at_peak_smoothed[cell_idx, -1]] = cell_ids[cell_idx]
+        else:
+            if len(ISI_at_peak_smoothed_labels[ISI_at_peak_smoothed[cell_idx, -1]].split(',')) == 3:
+                ISI_at_peak_smoothed_labels[ISI_at_peak_smoothed[cell_idx, -1]] += ',\n' + cell_ids[cell_idx]
+            else:
+                ISI_at_peak_smoothed_labels[ISI_at_peak_smoothed[cell_idx, -1]] += ', ' + cell_ids[cell_idx]
+    pl.figure()
+    for cell_idx in range(len(cell_ids)):
+        pl.plot(bin_widths, ISI_at_peak_smoothed[cell_idx, :], 'o-', color=colors[cell_idx], label=cell_ids[cell_idx],
+                markersize=4)
+
+    for label in ISI_at_peak_smoothed_labels.keys():
+        pl.annotate(ISI_at_peak_smoothed_labels[label], xy=(bin_widths[-1] + 0.05, label),
+                    horizontalalignment='left', verticalalignment='center', fontsize=8)
+    # pl.legend(loc='upper left', fontsize=8)
+    pl.xlim(bin_widths[0] - 0.05, bin_widths[-1] + 0.8)
+    pl.xlabel('Bin width (ms)')
+    pl.ylabel('ISI at peak (ms)')
+    pl.tight_layout()
+    if all_cells:
+        pl.savefig(os.path.join(save_dir_img, cell_type, 'ISI_at_peak_all_cells.png'))
+    else:
+        pl.savefig(os.path.join(save_dir_img, cell_type, 'ISI_at_peak.png'))
+
+    # fitting
+    pl.close('all')
+    if cell_type == 'grid_cells':
+        n_rows = 3
+        n_columns = 9
+        fig, axes = pl.subplots(n_rows, n_columns, sharex='all', sharey='all', figsize=(14, 8.5))
+        cell_idx = 0
+        for i1 in range(n_rows):
+            for i2 in range(n_columns):
+                if cell_idx < len(cell_ids):
+                    if get_celltype(cell_ids[cell_idx], save_dir) == 'stellate':
+                        axes[i1, i2].set_title(cell_ids[cell_idx] + ' ' + u'\u2605', fontsize=12)
+                    elif get_celltype(cell_ids[cell_idx], save_dir) == 'pyramidal':
+                        axes[i1, i2].set_title(cell_ids[cell_idx] + ' ' + u'\u25B4', fontsize=12)
+                    else:
+                        axes[i1, i2].set_title(cell_ids[cell_idx], fontsize=12)
+
+                axes[i1, i2].bar(1, (half_width_fit[cell_idx]), width=0.8, color='0.5')
+                axes[i1, i2].set_xlim(0, 2)
+                axes[i1, i2].set_xticks([])
+
+                if i2 == 0:
+                    axes[i1, i2].set_ylabel('Half width (ms)')
+                cell_idx += 1
+        pl.tight_layout()
+        pl.savefig(os.path.join(save_dir_img, 'half_width.png'))
+
+        fig, axes = pl.subplots(n_rows, n_columns, sharex='all', sharey='all', figsize=(14, 8.5))
+        cell_idx = 0
+        for i1 in range(n_rows):
+            for i2 in range(n_columns):
+                if cell_idx < len(cell_ids):
+                    if get_celltype(cell_ids[cell_idx], save_dir) == 'stellate':
+                        axes[i1, i2].set_title(cell_ids[cell_idx] + ' ' + u'\u2605', fontsize=12)
+                    elif get_celltype(cell_ids[cell_idx], save_dir) == 'pyramidal':
+                        axes[i1, i2].set_title(cell_ids[cell_idx] + ' ' + u'\u25B4', fontsize=12)
+                    else:
+                        axes[i1, i2].set_title(cell_ids[cell_idx], fontsize=12)
+
+                axes[i1, i2].bar(1, (ISI_at_peak_fit[cell_idx]), width=0.8, color='0.5')
+                axes[i1, i2].set_xlim(0, 2)
+                axes[i1, i2].set_xticks([])
+
+                if i2 == 0:
+                    axes[i1, i2].set_ylabel('ISI at peak (ms)')
+                cell_idx += 1
+        pl.tight_layout()
+        pl.savefig(os.path.join(save_dir_img, 'ISI_at_peak.png'))
+
+
+        fig, axes = pl.subplots(n_rows, n_columns, sharex='all', figsize=(14, 8.5))
+        cell_idx = 0
+        for i1 in range(n_rows):
+            for i2 in range(n_columns):
+                if cell_idx < len(cell_ids):
+                    if get_celltype(cell_ids[cell_idx], save_dir) == 'stellate':
+                        axes[i1, i2].set_title(cell_ids[cell_idx] + ' ' + u'\u2605', fontsize=12)
+                    elif get_celltype(cell_ids[cell_idx], save_dir) == 'pyramidal':
+                        axes[i1, i2].set_title(cell_ids[cell_idx] + ' ' + u'\u25B4', fontsize=12)
+                    else:
+                        axes[i1, i2].set_title(cell_ids[cell_idx], fontsize=12)
+
+                axes[i1, i2].bar(bins_fit[:-1], ISI_hist_fit[cell_idx], bins_fit[1] - bins_fit[0], color='0.5')
+                x = np.arange(0, 100, 0.01)
+                axes[i1, i2].plot(x, fun_fit(x, *p_opt_fit[cell_idx]), '-', color='k', linewidth=1.5)
+                axes[i1, i2].plot(ISI_at_peak_fit[cell_idx],
+                                  fun_fit(np.array([ISI_at_peak_fit[cell_idx]]), *p_opt_fit[cell_idx]), 'or')
+                axes[i1, i2].plot([root1_fit[cell_idx], root2_fit[cell_idx]],
+                                  [fun_fit(root1_fit[cell_idx], *p_opt_fit[cell_idx]),
+                                   fun_fit(root2_fit[cell_idx], *p_opt_fit[cell_idx])], 'r', linewidth=2)
+
+                if i1 == (n_rows - 1):
+                    axes[i1, i2].set_xlabel('ISI (ms)')
+                if i2 == 0:
+                    axes[i1, i2].set_ylabel('# ISIs')
+                cell_idx += 1
+        pl.tight_layout()
+        pl.savefig(os.path.join(save_dir_img, 'fit_ISI_hist.png'))
+        pl.show()
+
+    # pl.figure()
+    # pl.plot(range(len(cell_ids)), [p_opt_cells[c][0] for c in range(len(cell_ids))])
+    # pl.xticks(range(len(cell_ids)), cell_ids)
+    #
+    # pl.figure()
+    # pl.plot(range(len(cell_ids)), [p_opt_cells[c][1] for c in range(len(cell_ids))])
+    # pl.xticks(range(len(cell_ids)), cell_ids)
+    #
+    # pl.figure()
+    # pl.plot(range(len(cell_ids)), [p_opt_cells[c][2] for c in range(len(cell_ids))])
+    # pl.xticks(range(len(cell_ids)), cell_ids)
+    #
+    # pl.figure()
+    # pl.plot(range(len(cell_ids)), [p_opt_cells[c][3] for c in range(len(cell_ids))])
+    # pl.xticks(range(len(cell_ids)), cell_ids)
+    # pl.show()
--- analyze_domnisoru/spike_triggered_firing_rate_average.py
+++ analyze_domnisoru/spike_triggered_firing_rate_average.py
@@ -27,7 +27,10 @@ if __name__ == '__main__':
     after_AP = 200  # ms
 
     # over cells
-    firing_rate_cells = [0] * len(cell_ids)
+    avg_firing_rate_burst_cells = [0] * len(cell_ids)
+    std_firing_rate_burst_cells = [0] * len(cell_ids)
+    avg_firing_rate_single_cells = [0] * len(cell_ids)
+    std_firing_rate_single_cells = [0] * len(cell_ids)
     for cell_idx, cell_id in enumerate(cell_ids):
         print cell_id
         # load
@@ -39,19 +42,11 @@ if __name__ == '__main__':
         before_AP_idx = to_idx(before_AP, dt)
         after_AP_idx = to_idx(after_AP, dt)
 
-       # running firing rate
+        # identify single and burst APs
         if use_AP_max_idxs_domnisoru:
             AP_max_idxs = data['spiketimes']
         else:
             AP_max_idxs = get_AP_max_idxs(v, AP_thresholds[cell_id], dt)
-
-        firing_rate = init_nan(len(t))
-        for i, t_step in enumerate(t):
-            idx = np.logical_and(i - window_size_idx/2.0 <= AP_max_idxs, AP_max_idxs <= i + window_size_idx/2.0)
-            firing_rate[i] = len(AP_max_idxs[idx]) / (window_size/1000.0)
-        firing_rate_cells.append(firing_rate)
-
-        # identify single and burst APs
         ISIs = get_ISIs(AP_max_idxs, t)
         short_ISI_indicator = np.concatenate((ISIs <= ISI_burst, np.array([False])))
         starts_burst, ends_burst = get_start_end_group_of_ones(short_ISI_indicator.astype(int))
@@ -59,13 +54,32 @@ if __name__ == '__main__':
         AP_max_idxs_single = np.array(filter(lambda x: x not in AP_max_idxs[ends_burst + 1],
                                              AP_max_idxs[~short_ISI_indicator]))
 
+        # running firing rate
+        firing_rate = init_nan(len(t))
+        event_rate = init_nan(len(t))
+        fraction_burst = init_nan(len(t))
+        for i, t_step in enumerate(t):
+            idx = np.logical_and(i - window_size_idx/2.0 <= AP_max_idxs, AP_max_idxs <= i + window_size_idx/2.0)
+            idx_single = np.logical_and(i - window_size_idx / 2.0 <= AP_max_idxs_single,
+                                        AP_max_idxs_single <= i + window_size_idx / 2.0)
+            idx_burst = np.logical_and(i - window_size_idx / 2.0 <= AP_max_idxs_burst,
+                                       AP_max_idxs_burst <= i + window_size_idx / 2.0)
+            firing_rate[i] = len(AP_max_idxs[idx]) / (window_size/1000.0)
+            n_events = (len(AP_max_idxs_single[idx_single])+len(AP_max_idxs_burst[idx_burst]))
+            event_rate[i] = n_events / (window_size/1000.0)
+            if n_events != 0:
+                fraction_burst[i] = len(AP_max_idxs_burst[idx_burst]) / n_events
+
         # get firing rate for all APs
-        firing_rate_APs = find_all_AP_traces(firing_rate, before_AP_idx, after_AP_idx, AP_max_idxs_burst)
+        firing_rate_APs_burst = find_all_AP_traces(firing_rate, before_AP_idx, after_AP_idx, AP_max_idxs_burst)
+        firing_rate_APs_single = find_all_AP_traces(firing_rate, before_AP_idx, after_AP_idx, AP_max_idxs_single)
         t_AP = np.arange(after_AP_idx + before_AP_idx + 1) * dt - before_AP
 
         # average
-        avg_firing_rate = np.mean(firing_rate_APs, 0)
-        std_firing_rate = np.std(firing_rate_APs, 0)
+        avg_firing_rate_burst_cells.append(np.mean(firing_rate_APs_burst, 0))
+        std_firing_rate_burst_cells.append(np.std(firing_rate_APs_burst, 0))
+        avg_firing_rate_single_cells.append(np.mean(firing_rate_APs_single, 0))
+        std_firing_rate_single_cells.append(np.std(firing_rate_APs_single, 0))
 
         # save and plot
         save_dir_cell = os.path.join(save_dir_img, cell_type, cell_id)
@@ -79,35 +93,45 @@ if __name__ == '__main__':
         # pl.ylabel('Mem.Pot. (mV)')
         # pl.tight_layout()
         #
-        pl.figure()
-        pl.title(cell_id, fontsize=16)
-        pl.plot(t, firing_rate, 'k')
-        pl.xlabel('Time (ms)')
-        pl.ylabel('Firing rate (Hz)')
-        pl.tight_layout()
-        pl.show()
-
         # pl.figure()
-        # for fr in firing_rate_APs:
-        #     pl.plot(t_AP, fr)
+        # pl.title(cell_id, fontsize=16)
+        # pl.plot(t, firing_rate, 'k')
+        # pl.plot(t, event_rate, 'b')
         # pl.xlabel('Time (ms)')
         # pl.ylabel('Firing rate (Hz)')
         # pl.tight_layout()
-
+        # pl.show()
+        #
+        # pl.figure()
+        # pl.plot(t_AP, avg_firing_rate, 'k')
+        # pl.fill_between(t_AP, avg_firing_rate - std_firing_rate, avg_firing_rate + std_firing_rate, color='k',
+        #                 alpha=0.5)
+        # pl.xlabel('Time (ms)')
+        # pl.ylabel('Firing rate (Hz)')
+        # pl.tight_layout()
+        # pl.show()
+        bin_width_event = 2
+        steps = np.arange(0, 50, 1)
+        mean_frac = np.zeros(len(steps))
+        std_frac= np.zeros(len(steps))
+        for i, er in enumerate(steps):
+            idx = np.logical_and(er - bin_width_event/2.0 <= event_rate,  event_rate <= er + bin_width_event/2.0)
+            mean_frac[i] = np.nanmean(fraction_burst[idx])
+            std_frac[i] = np.nanstd(fraction_burst[idx])
         pl.figure()
-        pl.plot(t_AP, avg_firing_rate, 'k')
-        pl.fill_between(t_AP, avg_firing_rate - std_firing_rate, avg_firing_rate + std_firing_rate, color='k',
-                        alpha=0.5)
-        pl.xlabel('Time (ms)')
-        pl.ylabel('Firing rate (Hz)')
+        pl.title(cell_id, fontsize=16)
+        pl.plot(event_rate, fraction_burst, 'o', color='0.5', markersize=4)
+        pl.plot(steps, mean_frac, 'r')
+        pl.fill_between(steps, mean_frac - std_frac, mean_frac + std_frac, color='r', alpha=0.5)
+        pl.xlabel('Event rate (Hz)')
+        pl.ylabel('Fraction burst events')
         pl.tight_layout()
+        pl.savefig(os.path.join(save_dir_cell, 'event_rate_vs_fraction_burst.png'))
         pl.show()
-
 
     # save and plot
     pl.close('all')
     if cell_type == 'grid_cells':
-        # plot all return maps
         n_rows = 3
         n_columns = 9
         fig, axes = pl.subplots(n_rows, n_columns, sharex='all', sharey='all', figsize=(14, 8.5))
@@ -122,8 +146,10 @@ if __name__ == '__main__':
                     else:
                         axes[i1, i2].set_title(cell_ids[cell_idx], fontsize=12)
 
-                    axes[i1, i2].plot(ISIs_per_cell[cell_idx][:-1], ISIs_per_cell[cell_idx][1:], color='0.5',
-                                      marker='o', linestyle='', markersize=1, alpha=0.5)
+                    axes[i1, i2].plot(t_AP, avg_firing_rate_burst_cells[cell_idx], 'k')
+                    axes[i1, i2].fill_between(t_AP, avg_firing_rate_burst_cells[cell_idx] - std_firing_rate_burst_cells[cell_idx],
+                                              avg_firing_rate_burst_cells[cell_idx] + std_firing_rate_burst_cells[cell_idx],
+                                              color='k', alpha=0.5)
 
                     if i1 == (n_rows - 1):
                         axes[i1, i2].set_xlabel('ISI[n] (ms)')
@@ -137,5 +163,36 @@ if __name__ == '__main__':
                 cell_idx += 1
         pl.tight_layout()
         pl.subplots_adjust(wspace=0.25)
-        pl.savefig(os.path.join(save_dir_img, cell_type, 'sta_firing_rate.png'))
-        #pl.show()
+        pl.savefig(os.path.join(save_dir_img, cell_type, 'sta_firing_rate_burst.png'))
+
+        fig, axes = pl.subplots(n_rows, n_columns, sharex='all', sharey='all', figsize=(14, 8.5))
+        cell_idx = 0
+        for i1 in range(n_rows):
+            for i2 in range(n_columns):
+                if cell_idx < len(cell_ids):
+                    if get_celltype(cell_ids[cell_idx], save_dir) == 'stellate':
+                        axes[i1, i2].set_title(cell_ids[cell_idx] + ' ' + u'\u2605', fontsize=12)
+                    elif get_celltype(cell_ids[cell_idx], save_dir) == 'pyramidal':
+                        axes[i1, i2].set_title(cell_ids[cell_idx] + ' ' + u'\u25B4', fontsize=12)
+                    else:
+                        axes[i1, i2].set_title(cell_ids[cell_idx], fontsize=12)
+
+                    axes[i1, i2].plot(t_AP, avg_firing_rate_single_cells[cell_idx], 'k')
+                    axes[i1, i2].fill_between(t_AP, avg_firing_rate_single_cells[cell_idx] - std_firing_rate_single_cells[cell_idx],
+                                              avg_firing_rate_single_cells[cell_idx] + std_firing_rate_single_cells[cell_idx],
+                                              color='k', alpha=0.5)
+
+                    if i1 == (n_rows - 1):
+                        axes[i1, i2].set_xlabel('ISI[n] (ms)')
+                    if i2 == 0:
+                        axes[i1, i2].set_ylabel('ISI[n+1] (ms)')
+                else:
+                    axes[i1, i2].spines['left'].set_visible(False)
+                    axes[i1, i2].spines['bottom'].set_visible(False)
+                    axes[i1, i2].set_xticks([])
+                    axes[i1, i2].set_yticks([])
+                cell_idx += 1
+        pl.tight_layout()
+        pl.subplots_adjust(wspace=0.25)
+        pl.savefig(os.path.join(save_dir_img, cell_type, 'sta_firing_rate_single.png'))
+        pl.show()
